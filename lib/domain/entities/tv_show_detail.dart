5f8707e38d04bbec51090c4c3e705074ff57d882
blitzkz23 <blitzaldykazu@gmail.com>
2022-03-19 19:14:16 +0700

    Adjustment for tv show detail entity and raw models

--- lib/domain/entities/tv_show_detail.dart
+++ lib/domain/entities/tv_show_detail.dart
@@ -5,88 +5,55 @@ class TvShowDetail extends Equatable {
   TvShowDetail({
     required this.adult,
     required this.backdropPath,
-    required this.createdBy,
-    required this.episodeRunTime,
     required this.firstAirDate,
     required this.genres,
-    required this.homepage,
     required this.id,
-    required this.inProduction,
-    required this.languages,
     required this.lastAirDate,
     required this.name,
-    required this.nextEpisodeToAir,
     required this.numberOfEpisodes,
     required this.numberOfSeasons,
-    required this.originCountry,
     required this.originalLanguage,
     required this.originalName,
     required this.overview,
     required this.popularity,
     required this.posterPath,
-    required this.productionCompanies,
-    required this.status,
-    required this.tagline,
-    required this.type,
     required this.voteAverage,
     required this.voteCount,
   });
 
-  bool adult;
-  String backdropPath;
-  List<dynamic> createdBy;
-  List<int> episodeRunTime;
-  DateTime firstAirDate;
-  List<Genre> genres;
-  String homepage;
-  int id;
-  bool inProduction;
-  List<String> languages;
-  DateTime lastAirDate;
-  String name;
-  dynamic nextEpisodeToAir;
-  int numberOfEpisodes;
-  int numberOfSeasons;
-  List<String> originCountry;
-  String originalLanguage;
-  String originalName;
-  String overview;
-  double popularity;
-  String posterPath;
-  List<dynamic> productionCompanies;
-  String status;
-  String tagline;
-  String type;
-  double voteAverage;
-  int voteCount;
+  final bool adult;
+  final String backdropPath;
+  final DateTime firstAirDate;
+  final List<Genre> genres;
+  final int id;
+  final DateTime lastAirDate;
+  final String name;
+  final int numberOfEpisodes;
+  final int numberOfSeasons;
+  final String originalLanguage;
+  final String originalName;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final double voteAverage;
+  final int voteCount;
 
   @override
   List<Object?> get props => [
         adult,
         backdropPath,
-        createdBy,
-        episodeRunTime,
         firstAirDate,
         genres,
-        homepage,
         id,
-        inProduction,
-        languages,
         lastAirDate,
         name,
-        nextEpisodeToAir,
         numberOfEpisodes,
         numberOfSeasons,
-        originCountry,
         originalLanguage,
         originalName,
         overview,
         popularity,
         posterPath,
-        productionCompanies,
-        status,
-        tagline,
-        type,
         voteAverage,
         voteCount,
       ];
