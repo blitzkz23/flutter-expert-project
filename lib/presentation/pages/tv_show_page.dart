8ad1ff8f1afca349109cc6365b0fa0eccd45376d
blitzkz23 <blitzaldykazu@gmail.com>
2022-03-29 12:20:41 +0700

    Implemented top rated tv show section of presentation layer

--- lib/presentation/pages/tv_show_page.dart
+++ lib/presentation/pages/tv_show_page.dart
@@ -4,6 +4,7 @@ import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/domain/entities/tv_show.dart';
 import 'package:ditonton/presentation/pages/popular_tv_shows_page.dart';
 import 'package:ditonton/presentation/pages/search_page.dart';
+import 'package:ditonton/presentation/pages/top_rated_tv_shows_page.dart';
 import 'package:ditonton/presentation/provider/tv_show_list_notifier.dart';
 import 'package:ditonton/presentation/widgets/custom_drawer.dart';
 import 'package:flutter/material.dart';
@@ -84,7 +85,8 @@ class _TvShowPageState extends State<TvShowPage> {
                   }),
                   _buildSubHeading(
                     title: 'Top Rated',
-                    onTap: () {},
+                    onTap: () => Navigator.pushNamed(
+                        context, TopRatedTvShowsPage.ROUTE_NAME),
                   ),
                   Consumer<TvShowListNotifier>(builder: (context, data, child) {
                     final state = data.topRatedState;
